78731d96f934d99db139b3962482ff9c6c9a1808
Natalia Vudoyra <naty.vudoyra@hotmail.com>
2026-06-09 19:30:22 -0600

    Improve format output

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,13 +1,14 @@
 print("Bill Split Calculator")
 
-bill_amount = float(input())
-tip_percentage = float(input())
-number_of_people = int(input())
+bill_amount = float(input("Enter bill amount: "))
+tip_percentage = float(input("Enter tip percentage: "))
+number_of_people = int(input("Enter number of people: "))
 
 tip_amount = (tip_percentage / 100) * bill_amount
+total_amount = bill_amount + tip_amount
 
-total_amount = tip_amount + bill_amount
-print(f"Total (including tip): ${total_amount}")
+print(f"\nTotal (including tip): ${total_amount:.2f}")
 
 amount_per_person = total_amount / number_of_people
-print(f"Each person pays: ${amount_per_person}")
+
+print(f"Each person pays: ${amount_per_person:.2f}")
